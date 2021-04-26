e0df3de0a92463e5bab052e2f1c6f811fa676576
Lucas Davi Possati de Souza <lucasdavipossati@hotmail.com>
2021-04-26 17:20:23 -0300

    Personalizações na tela de login

---
+++ lib/login.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iWash/home.dart';
+
+import 'cadastrar.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  var _txtLogin = TextEditingController();
+  var _txtSenha = TextEditingController();
+  var _usuarioLogado = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          constraints: BoxConstraints(maxWidth: 430, maxHeight: 650),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                      'assets/images/background/background-image.jpg'),
+                  fit: BoxFit.cover),
+              borderRadius: new BorderRadius.circular(26.0)),
+          padding: EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 150,
+                child: ClipRRect(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Image.asset('assets/images/login/iconProfile.png',
+                        width: 140),
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 18.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, digite um e-mail ou usuário!';
+                    }
+                    return null;
+                  },
+                  textAlign: TextAlign.center,
+                  controller: _usuarioLogado,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.account_circle),
+                      labelText: 'E-mail'),
+                ),
+              ),
+              SizedBox(height: 5),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                controller: _txtSenha,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock), labelText: 'Senha'),
+              ),
+              SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 37,
+                  child: ElevatedButton.icon(
+                    label: Text('Entrar'),
+                    icon: Icon(Icons.login),
+                    onPressed: () {
+                      // Navigator.pushNamed(context, '/home');                  
+                      setState(() {
+                        var nomeUsuario = UsuarioLogado(_usuarioLogado.text);
+
+                        Navigator.pushNamed(context, '/navegacao',
+                            arguments: nomeUsuario);
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red[500],
+                      textStyle: TextStyle(fontSize: 15),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(45.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              // Padding(
+              //   padding: const EdgeInsets.all(8.0),
+              //   child: SizedBox(
+              //     width: double.infinity,
+              //     height: 33,
+              //     child: ElevatedButton.icon(
+              //       label: Text('Esqueci a senha'),
+              //       icon: Icon(Icons.password),
+              //       onPressed: () {
+              //         Navigator.pushNamed(context, '/esqueceSenha');
+              //       },
+              //       style: ElevatedButton.styleFrom(
+              //           primary: Colors.red[500],
+              //           textStyle: TextStyle(fontSize: 15)),
+              //     ),
+              //   ),
+              // ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 37,
+                  child: ElevatedButton.icon(
+                    label: Text('Logar com o Facebook'),
+                    icon: Icon(Icons.facebook),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue[500],
+                      textStyle: TextStyle(fontSize: 15),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(45.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: TextButton(
+                  child: Text(
+                    "Esqueci a senha",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/esqueceSenha');
+                  },
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 25.0),
+                child: Text(
+                  "Ainda não é registrado ?",
+                  textAlign: TextAlign.right,
+                  style: GoogleFonts.lato(color: Colors.white),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(right: 12.0, left: 12.0),
+                child: TextButton(
+                  child: Text(
+                    "Registre-se",
+                    textAlign: TextAlign.right,
+                    style: GoogleFonts.lato(
+                        color: Colors.white24,
+                        decoration: TextDecoration.underline),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/cadastrar');
+                  },
+                ),
+              ),
+
+              // if(msg?.primeiroNome != null){
+              //   AlertDialog(
+              //   title: Text("Usuário cadastrado com sucesso!"),
+              //   content: Text(msg?.primeiroNome ?? '',
+              //       style: TextStyle(fontSize: 15)),
+
+              //   actions: [
+              //     ElevatedButton(
+              //       child: Text("Sim"),
+              //       onPressed: () {
+              //         Navigator.pushNamed(context, '/navegacao');
+              //       },
+              //     ),
+              //     ElevatedButton(
+              //       child: Text("Não"),
+              //       onPressed: () {
+              //         Navigator.pushNamed(context, '/login');
+              //       },
+              //     ),
+              //   ],
+              //   elevation: 24.0,
+              // ),
+              // },
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class UsuarioLogado {
+  final String usuarioLogado;
+  UsuarioLogado(this.usuarioLogado);
+}
--- lib/pesquisar.dart
+++ lib/pesquisar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'lojaCard.dart';
+import 'lojas.dart';
 
 class Pesquisar extends StatefulWidget {
   @override
@@ -8,121 +10,268 @@ class Pesquisar extends StatefulWidget {
 }
 
 class _PesquisarState extends State<Pesquisar> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Container(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 20, left: 8, right: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Center(
-                        child: Text(
-                          'Categorias',
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.lato(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 100,
-                            height: 100,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                image: AssetImage('assets/images/categorias/carros.jpg'),
-                                fit: BoxFit.fill
-                              ),
-                            ),
-                          ),
-                        ),
-                        
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 100,
-                            height: 100,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                image: NetworkImage('assets/images/perfil.jpg'),
-                                fit: BoxFit.fill
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+    List<Lojas> _listLojas = [];
+    _listLojas = [];
+    _listLojas.add(Lojas(
+        logoEmpresa: 'assets/images/Wash/wash1.jpg',
+        nomeLoja: 'Loja Automotiva',
+        avaliacao: '4.8 ',
+        km: '3.3 km',
+        tempoBusca: '2 hrs',
+        precoBusca: '3,00'));
+    _listLojas.add(Lojas(
+        logoEmpresa: 'assets/images/Wash/wash2.jpg',
+        nomeLoja: 'Loja Automotiva',
+        avaliacao: '4.8 ',
+        km: '3.3 km',
+        tempoBusca: '2 hrs',
+        precoBusca: '3,00'));
+
+    _listLojas.add(Lojas(
+        logoEmpresa: 'assets/images/Wash/wash3.jpg',
+        nomeLoja: 'Loja Automotiva',
+        avaliacao: '4.8 ',
+        km: '3.3 km',
+        tempoBusca: '2 hrs',
+        precoBusca: '3,00'));
+
+    _listLojas.add(Lojas(
+        logoEmpresa: 'assets/images/Wash/wash4.jpg',
+        nomeLoja: 'Loja Automotiva',
+        avaliacao: '4.8 ',
+        km: '3.3 km',
+        tempoBusca: '2 hrs',
+        precoBusca: '3,00'));
+
+    _listLojas.add(Lojas(
+        logoEmpresa: 'assets/images/Wash/wash5.jpg',
+        nomeLoja: 'Loja Automotiva',
+        avaliacao: '4.8 ',
+        km: '3.3 km',
+        tempoBusca: '2 hrs',
+        precoBusca: '3,00'));
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        Container(
+          margin: EdgeInsets.only(right: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                height: 150,
+                margin: EdgeInsets.only(top: 10),
+                padding: EdgeInsets.only(top: 12, left: 12),
+                alignment: Alignment.topLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Column(
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            'Categorias',
+                            style: GoogleFonts.lato(
+                                fontWeight: FontWeight.bold, fontSize: 17),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
                       children: [
                         Padding(
-                          padding: const EdgeInsets.only(left: 22.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
                             children: [
-                              Center(
-                                  child: Text('Carros',
-                                      style: GoogleFonts.lato(
-                                          fontSize: 16,
-                                          color: Colors.grey,
-                                          fontWeight: FontWeight.w700))),
+                              Container(
+                                margin: EdgeInsets.only(bottom: 10),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.asset(
+                                      'assets/images/Categorias/carros.jpg',
+                                      height: 60),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Carros',
+                                    style: GoogleFonts.lato(
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(bottom: 10),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.asset(
+                                      'assets/images/Categorias/caminhao.jpg',
+                                      height: 60),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Caminhão',
+                                    style: GoogleFonts.lato(
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(bottom: 10),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.asset(
+                                      'assets/images/Categorias/motos.jpg',
+                                      height: 60),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Motos',
+                                    style: GoogleFonts.lato(
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(bottom: 10),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.asset(
+                                      'assets/images/Categorias/jet.jpg',
+                                      height: 60),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Carros',
+                                    style: GoogleFonts.lato(
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(bottom: 10),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.asset(
+                                      'assets/images/Categorias/onibus.png',
+                                      height: 60),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Ônibus',
+                                    style: GoogleFonts.lato(
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ],
                           ),
                         ),
                       ],
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 22.0),
-                          child: Row(
-                            children: [
-                              Center(
-                                  child: Text('Guilherme Brito',
-                                      style: GoogleFonts.lato(
-                                          fontSize: 16,
-                                          color: Colors.grey,
-                                          fontWeight: FontWeight.w700))),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    
                   ],
                 ),
-              ],
-            ),
+              ),
+              // Container(
+              //   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              //   child: Column(
+              //     children: <Widget>[
+              //       Form(
+              //         key: _formKey,
+              //         child: Row(
+              //           children: <Widget>[
+              //             Expanded(
+              //                 child: TextFormField(
+              //               style:
+              //                   TextStyle(fontSize: 24, color: Colors.black38),
+              //               decoration: InputDecoration(
+              //                   hintText: "Pesquise sua loja",
+              //                   hintStyle: TextStyle(fontSize: 18)),
+              //               keyboardType: TextInputType.text,
+              //             )),
+              //             Container(
+              //                 margin: EdgeInsets.only(left: 20),
+              //                 child: ElevatedButton(
+              //                     child: Icon(Icons.search),
+              //                     onPressed: () {
+              //                       print("Clicado");
+              //                     },
+              //                     style: ButtonStyle(
+              //                       backgroundColor:
+              //                           MaterialStateProperty.all<Color>(
+              //                               Colors.blue),
+              //                     )))
+              //           ],
+              //         ),
+              //       ),
+              //       SizedBox(
+              //           child: ListView.builder(
+              //         scrollDirection: Axis.vertical,
+              //         shrinkWrap: true,
+              //         itemCount: _listLojas.length,
+              //         itemBuilder: (context, index) =>
+              //             LojaCard(lojas: _listLojas[index]),
+              //       ))
+              //     ],
+              //   ),
+              // ),
+            ],
           ),
         ),
-      ),
+      ],
     );
   }
 }
